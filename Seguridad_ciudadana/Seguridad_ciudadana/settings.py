8a7a1f4ec9f7c8e1ebd6f4f404d1c7297203a4f0
benjaCeron <benjaceron1412@gmail.com>
2025-10-28 23:02:56 -0300

    prueba

--- Seguridad_ciudadana/Seguridad_ciudadana/settings.py
+++ Seguridad_ciudadana/Seguridad_ciudadana/settings.py
@@ -141,6 +141,35 @@ SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
+DEBUG = True
+
+# Logging detallado
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django.template': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
+
 # ---------------- CORS ----------------
 if DEBUG:
     CORS_ALLOW_ALL_ORIGINS = True
